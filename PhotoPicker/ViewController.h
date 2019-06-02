3866d63da8eabd4eb71a24eff1d11c1010013b21
DmitryNosko <dmitry.w.nosko@gmail.com>
2019-06-02 11:56:52 +0300

    Photo picker initial commit. SVA created. .gitignore added

---
+++ PhotoPicker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PhotoPicker
+//
+//  Created by Dima  on 6/2/19.
+//  Copyright © 2019 Dima . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
